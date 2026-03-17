82cae6794a7889a8e53447d62980176c973cd281
RenoCsn <csnt.ren@gmail.com>
2026-03-17 16:08:16 -0300

    feat: type description in typography stories

--- src/stories/Components/Atoms/index.stories.tsx
+++ src/stories/Components/Atoms/index.stories.tsx
@@ -5,6 +5,33 @@ import { TypographyComponent } from '../../../Components/Typography/index'
 const meta = {
   title: 'Atoms/Typography',
   component: TypographyComponent,
+  argTypes: {
+    color: {
+      control: {
+        type: 'color',
+      },
+      table: {
+        type: {
+          summary:
+            'primary, secondary, success, error, info, warning, textPrimary, textSecondary,textDisabled ou código hex',
+        },
+      },
+    },
+    align: {
+      table: {
+        type: {
+          summary: 'center, start, justify, left, right, end',
+        },
+      },
+    },
+    variant: {
+      table: {
+        type: {
+          summary: 'h1, h2, h3, h4, h5, p, span, b',
+        },
+      },
+    },
+  },
 } satisfies Meta<typeof TypographyComponent>
 
 export default meta
@@ -13,27 +40,36 @@ type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
   args: {
-    children: 'Default',
+    children: 'Default.',
   },
 }
 
 export const Color: Story = {
+  parameters: {
+    docs: {
+      description: {
+        storybook:
+          'Typography accepts primary, secondary, success, error, info, warning, textPrimary, textSecondary, textDisabled as preset colors.',
+      },
+    },
+  },
   args: {
-    children: 'Typography with other color',
+    children: 'Typography with preset color',
     color: 'success',
   },
 }
 
 export const CustomColor: Story = {
   args: {
-    children: 'Typography with a custom color',
+    children:
+      'Typography also can be used with a custom color passing the hex code.',
     color: '#0502c6',
   },
 }
 
 export const Align: Story = {
   args: {
-    children: 'Typography with align',
+    children: 'Typography with align style.',
     align: 'center',
   },
 }
@@ -41,14 +77,14 @@ export const Align: Story = {
 //Terminar
 export const Variant: Story = {
   args: {
-    children: 'H1 typography',
+    children: 'Typography as H1 style.',
     variant: 'h1',
   },
 }
 
 export const Custom: Story = {
   args: {
-    children: 'Typography with custom css',
+    children: 'Typography with custom css.',
     classNameCustom: 'underline font-black!',
   },
 }
